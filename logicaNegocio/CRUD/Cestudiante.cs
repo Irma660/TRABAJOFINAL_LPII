2c131948bde88691d06aba56173c8c33aed1b59d
irma660 <1325455@senati.pe>
2023-09-20 18:16:45 -0500

    uniendo capa negocio con acceso de datos

---
+++ logicaNegocio/CRUD/Cestudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using accesoDatos.entidades;
+
+namespace logicaNegocio.CRUD
+{
+    public class Cestudiante
+    {
+        estudiante persona = new estudiante();
+        public DataTable Mostrar()
+        {
+            DataTable td = new DataTable();
+            td = persona.Mostrar();
+            return td;
+
+        }
+    }
+}
