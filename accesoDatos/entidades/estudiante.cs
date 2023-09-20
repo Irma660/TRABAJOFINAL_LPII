6a50cdc31389024614da46aa70bbd918d7f23551
irma660 <1325455@senati.pe>
2023-09-20 17:54:58 -0500

    creando variables

---
+++ accesoDatos/entidades/estudiante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using accesoDatos.Database;
+
+namespace accesoDatos.entidades
+{
+    public class estudiante
+    {
+        //variables
+        DataBase c = new DataBase();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+        DataTable td = new DataTable();
+    }
+}
